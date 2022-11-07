cb27ae89200320ee22bbabd73e0416e431cb7ebc
purwarshivani <shivani.purwar@infodevelopers.local>
2022-11-07 12:55:05 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Form from './Form';
+import Parent from './withProps';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+// function App() {
+// const [count, setCount] = React.useState(0)
+// //  const handleClick=()=>{
+// //     alert('hey hitted')
+// //   }
+//   return (
+//     <div className="App">
+//       <p>count Reached {count}</p>
+//       <button onClick={()=>setCount(count+1)}>Click Me</button>
+    
+//     </div>
+//   );
+// }
+
+class App extends Component{
+  state={
+    count :10
+  }
+
+//   decrease =()=>{
+//     if(this.state.count > 0)
+//     {
+//  this.setState({count: this.state.count -1})
+//     }
+//   }
+
+  render(){
+    return(
+      <>
+      <div>
+        {/* <p>Count Reached {this.state.count}</p>
+        <button onClick={()=> this.decrease()}> Click </button> */}
+        {/* <Parent/> */}
+        <Form/>
+
+        
+      </div>
+      </>
+    )
+  }
 }
 
 export default App;
